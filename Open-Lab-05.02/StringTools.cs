98f17514d02ebecdc33e8f375b1fc9eb416d9a43
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 20:19:10 +0100

    mam

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -6,7 +6,41 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            var list = new List<char>();
+            char[] characters = sentence.ToCharArray();
+            char to_Add = '\0'; 
+            bool can_Add = false;
+
+            for (int i = characters.Length - 1; i >= 0; i -= 1)
+            {
+                string character = characters[i].ToString();
+                if (character == "?" || character == "!" )
+                {
+                    if (can_Add == false)
+                    {
+                        to_Add = characters[i];
+                    }
+                    else
+                    {
+                        list.Add(characters[i]);
+                    }
+
+                }
+                else
+                {
+                    can_Add = true;
+                    list.Add(characters[i]);
+                } 
+            }
+
+            list.Reverse();
+
+            if (to_Add.ToString() != "\0")
+            {
+                list.Add(to_Add);
+            }
+
+            return new string(list.ToArray());
         }
     }
 }
